a8907fd2dd72352fdd210210b92d46621fc33651
Tr15unu <43811117+Tr15unu@users.noreply.github.com>
2019-05-22 14:04:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ oc delete project mssql
 sleep 60
 oc new-project mssql 
 oadm policy add-scc-to-user anyuid -z default
-oc new-app https://github.com/glennswest/mssqlrhel 
-oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123!
+oc new-app https://github.com/Tr15unu/mssqlrhel
+oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD=password
 oc volume "dc/mssqlrhel" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=mssqlrhel --claim-mode="ReadWriteOnce" --claim-size=1G
 sleep 20
 oc logs -f bc/mssqlrhel
